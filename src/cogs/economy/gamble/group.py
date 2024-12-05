a7b3a7441eade339562197d37d1efd44a23052bd
Cromatin <lars-winzer@web.de>
2024-12-05 12:35:05 +0100

    Added the betflip command to the gamble group
    Commit for #11

--- src/cogs/economy/gamble/group.py
+++ src/cogs/economy/gamble/group.py
@@ -16,14 +16,29 @@ class Gamble_CommandGroup(Base_GroupCog, group_name='gamble'):
     @app_commands.describe(bet = "Amount you would like to gamble with")
     async def wheel(self, ctx: discord.Interaction, bet:int):
         await self.__wheel_impl.on_command(ctx, bet)
+    
+    @app_commands.command(name = "betflip", description = "Bet to guess will the result be heads or tails")
+    @app_commands.describe(bet = "Amount you would like to gamble with", guess = "Your guess")
+    @app_commands.choices(
+        guess = [
+            app_commands.Choice(name = "Heads", value = 0),
+            app_commands.Choice(name = "Tails", value = 1)
+        ]
+    )
+    async def betflip(self, ctx: discord.Interaction, bet:int, guess:int):
+        await self.__betflip_impl.on_command(ctx, bet, guess)
 
     async def cog_load(self):
         await self.__bot.load_extension("cogs.economy.gamble.impls.wheel_impl")
+        await self.__bot.load_extension("cogs.economy.gamble.impls.betflip_impl")
         self.__wheel_impl = Wheel_Impl(self.__bot)
+        self.__betflip_impl = Betflip_Impl(self.__bot)
+
         return await super().cog_load()
 
     async def cog_unload(self):
         await self.__bot.unload_extension("cogs.economy.gamble.impls.wheel_impl")
+        await self.__bot.unload_extension("cogs.economy.gamble.impls.betflip_impl")
         return await super().cog_unload()
 
 async def setup(bot: commands.Bot):
