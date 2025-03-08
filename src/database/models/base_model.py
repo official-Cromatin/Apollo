b028b8d6a9b8b3cbe8d102808715a2e29834de8f
official-Cromatin <lars-winzer@web.de>
2025-03-08 19:33:21 +0100

    Drasticly improved the docstrings of the existing functions. Added deletion tracking and exposed the id of the row

--- src/database/models/base_model.py
+++ src/database/models/base_model.py
@@ -7,6 +7,7 @@ class Base_Model(ABC):
     def __init__(self, database_connection:Connection):
         self._connection = database_connection
         self._id:int
+        self._deleted = False
     
     @abstractmethod
     def __str__(self) -> str:
@@ -14,24 +15,56 @@ class Base_Model(ABC):
     
     @abstractmethod
     @classmethod
-    async def create(self, *args) -> "Base_Model":
-        """Creates a new entry in the database"""
+    async def create(cls, *args) -> "Base_Model":
+        """Creates a new entry in the database
+        
+        :return:
+            An instance of User_Currency with the specified data
+        
+        :raises AlreadyExists:
+            If the user currency already exists in the database"""
         ...
 
     @abstractmethod
     @classmethod
-    async def load(self, *args) -> "Base_Model":
-        """Loads an existing entry from the database"""
+    async def load(cls, *args) -> "Base_Model":
+        """Loads an existing entry from the database
+        
+        :return: 
+            An instance of User_Currency with the loaded data
+        
+        :raises NotFound:
+            If the user currency is not found in the database"""
         ...
     
     @abstractmethod
-    async def save(self):
-        """Updates the saved data, regardless of whether the values have changed or not"""
+    async def save(self) -> "Base_Model":
+        """Updates the saved data, regardless of whether the values have changed or not
+
+        :returns self:
+            The object itself, to allow for chaining
+
+        :raises AlreadyExists:
+            The entry already exists
+
+        :raises NoLongerExists:
+            The entry has been deleted
+        """
         ...
 
     @abstractmethod
-    async def delete(self):
-        """Removes the entry from the table, the data in the properties of the object is retained"""
+    async def delete(self) -> "Base_Model":
+        """Removes the entry from the table, the data in the properties of the object is retained
+        
+        :returns self:
+            The object itself, to allow for chaining
+        
+        :raises NoLongerExists:
+            The entry has already been deleted
+
+        :raises NotFound:
+            The entry has not been found
+        """
         ...
 
     @abstractmethod
@@ -43,3 +76,13 @@ class Base_Model(ABC):
     def data(self) -> dict[str, Any]:
         """Returns the data that the entry stores in the database (excludes the arguments for unique identification)"""
         ...
+    
+    @property
+    def id(self) -> int:
+        """Unique identifier (primary key) of the entry"""
+        return self._id
+    
+    @property
+    def deleted(self) -> bool:
+        """Indicates whether the entry has been deleted"""
+        return self._deleted
