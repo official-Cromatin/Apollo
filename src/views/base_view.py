02604e0da09aeaa0505f6ba92e387091da8ba17c
official-Cromatin <lars-winzer@web.de>
2025-05-01 18:35:11 +0200

    Updated the base_view class to (in theory) restore both ui components and attributes

--- src/views/base_view.py
+++ src/views/base_view.py
@@ -1,13 +1,22 @@
 import discord
-from abc import ABC, abstractmethod
+from abc import ABC
 from bot import Apollo_Bot
 from typing import Any, Union, Callable
-from views.exceptions import AlreadyActive, StoppedBefore, AlreadyStopped, NotActive
+from views.exceptions import AlreadyActive, StoppedBefore, AlreadyStopped, NotActive, CallbackNotFound, AttributeNameConflict, AttributeNotFound
 from database.models.saved_state.model import Saved_State
 import logging
 
 class Base_View(discord.ui.View, ABC):
-    """Base class wich all views have to inherit"""
+    """Base class wich all views have to inherit
+    
+    To create a restorable view, you have to implement the following methods:
+    | Function name         | Optional   | Description                                                      |
+    |-----------------------|------------|------------------------------------------------------------------|
+    | `new`                 | `false`    | Creates a new view and sets the view specific attributes         |
+    | `restore`             | `true`     | Restores a existing view, restoring the values of all attributes |
+    | `save_state`          | `true`     | Saves the state (values of the attributes) of the view           |
+    | `identify_callback`   | `true`     | Identified the matching callback, based on the custom_id         |
+    """
     _view_name:str = None
 
     def __init_subclass__(cls, view_name:str):
@@ -35,21 +44,71 @@ class Base_View(discord.ui.View, ABC):
         self._channel_id = channel_id
         self._message_id = message_id
         self._bot = bot
-        self._active:bool = False
-        self._stopped:bool = False
-        self._database_id:int = None
+        self._active = False
+        self._stopped = False
+        self._saved_state:Saved_State = None
+        self._view_data_attribute_names:list[str] = []
 
     def new(self, *args, **kwargs) -> "Base_View":
         """Creates a new view, responsible for setting all initial values"""
         logging.getLogger("view").info(f"New view (timeout={self.timeout}) created on guild {self._guild_id} in channel {self._channel_id}", extra={"iname": self._view_name, "id": self._message_id})
 
-    def restore(state:Saved_State, message:discord.Message) -> "Base_View":
-        """Restores a existing view from the given state"""
+    def restore(self, state:Saved_State, message:discord.Message) -> "Base_View":
+        """Restores a existing view from the given state
+
+        *Note: The restore of all attributes and *
+
+        Warning: If the custom_id of the element has no matching function, the reconstruction for this component will fail!
+        
+        :param Saved_State state:
+            Saved state model containing saved data
+            
+        :param discord.Message message:
+            Message object from the interaction
+            
+        :param Apollo_Bot bot:
+            Bot instance"""
+        # Restore the ui components themself
+        action_row_number = 0
+        for action_row in message.components:
+            for component in action_row.children:
+
+                # Attempt to restore the callback
+                try:
+                    callback = self.identify_callback(restored_component.custom_id)
+                except CallbackNotFound:
+                    logging.getLogger("view").error(f"Failed to restore callback for ui element with custom_id={restored_component.custom_id}!", extra={"iname": state.view_name, "id": state.message_id})
+
+                # Restore component and add to view
+                restored_component = self.restore_component(component, callback, action_row_number)
+                self.add_item(restored_component)
+            action_row_number += 1
+
+        # Restore the saved attributes
+        self.set_attributes(state.state_data)
+
+        self._saved_state = state
         logging.getLogger("view").info(f"Existing view restored on guild ({state.guild_id}) in channel {state.channel_id}", extra={"iname": state.view_name, "id": state.message_id})
 
-    @abstractmethod
+    async def save_state(self):
+        """Saves the state the view is currently in
+        
+        Note: It respects ONLY attributes specified in `_view_data` (name mangling must not be accounted for)"""
+        view_data = self.get_attributes(self._view_data_attribute_names)
+
+        # If no attributes are set, insert NULL in the column as a representation of no values being specified
+        if len(view_data) == 0:
+            view_data = None
+        
+        if self._saved_state is None:
+            self._saved_state = await Saved_State.create(self._bot.database, self._guild_id, self._channel_id, self._message_id, view_data, self._view_name, self.timeout)
+        
+        logging.getLogger("view").debug(f"Saved view", extra={"iname": self._view_name, "id": self._message_id})
+
     def identify_callback(self, custom_id:str) -> function:
         """Returns the callback method for the matching custom_id
+
+        Override if you have custom_ids that dont match the name of the callback function BEFORE calling this super function
         
         :param str custom_id:
             Custom id specified at the creation of the ui element
@@ -60,11 +119,73 @@ class Base_View(discord.ui.View, ABC):
         :raise CallbackNotFound:
             The requested `custom_id` is unknown to this view
         """
-        ...
+        if hasattr(self, custom_id):
+            callback = getattr(self, custom_id)
+            if callable(callable):
+                return callback
 
-    async def save_state(self):
-        """Saves the state the view is currently in"""
-        logging.getLogger("view").debug(f"Saved view")
+        if self._saved_state is None:
+            raise CallbackNotFound(self._view_name, custom_id)
+        raise CallbackNotFound(self._view_name, custom_id)
+
+    def get_attributes(self, filter:list[str] = None, unmangle_attribute_name:bool = False) -> dict[str, Any]:
+        """Returns the view specific attributes, as defined in `view_data_attribute_names`
+        
+        :param list[str] filter:
+            If specified, only attributes that are specified by name are stored in the Dict
+
+            Warning: If unmangling is deactivated, the attribute name must be mangled
+
+        :param unmangle_attribute_name bool:
+            The attribute names in the dict will be unmangled (Might lead to conflics and might not be restorable)
+
+        :raise AttributeNameConflict:
+            An attribute with the same name has already been saved in the dict.
+
+            *Note: This can be prevented by `unmangle_attribute_name` = `False`*
+
+        :return:
+            Dictionary containing the names of the attributes and its respective value"""
+        view_data:Union[dict[str, Any] | None] = {}
+        for cls in self.__class__.mro():
+            if cls is object:
+                continue
+
+            class_name = cls.__name__
+            for attr in self.__dict__:
+                value = getattr(self, attr)
+
+                # Unmangle attribute_name
+                if unmangle_attribute_name:
+                    if attr.startswith(f"_{class_name}__"):
+                        attribute_name = attr.removeprefix(f"_{class_name}__")
+                    else:
+                        attribute_name = attr
+                else:
+                    if attr.startswith(f"_{class_name}__"):
+                        short_name = attr.removeprefix(f"_{class_name}__")
+                        attribute_name = f"{class_name}.__{short_name}"
+                    else:
+                        attribute_name = f"{class_name}.{attr}"
+
+                if attribute_name in self._view_data_attribute_names:
+                    if attribute_name in view_data:
+                        raise AttributeNameConflict(self._view_name, attribute_name)
+                    view_data[attribute_name] = value
+        return view_data
+    
+    def set_attributes(self, attribute_data:dict[str, Any]):
+        """Restores the value of the attributes specified in the dictionary
+
+        *Note: Only unmangled values will function properly*
+        
+        :param dict[str, Any] attribute_data:
+            The values of the attributes to be restored"""
+        for attribute_name, attribute_value in attribute_data.items():
+            if hasattr(self, attribute_name):
+                setattr(self, attribute_name, attribute_value)
+            else:
+                raise AttributeNotFound(self._view_name, attribute_name)
 
     @staticmethod
     def restore_component(component:Union[discord.Button, discord.SelectMenu, discord.TextInput], callback:Callable, row:int = None) -> Union[discord.ui.Button, discord.ui.Select, discord.ui.UserSelect, discord.ui.RoleSelect, discord.ui.MentionableSelect, discord.ui.TextInput]:
@@ -81,6 +202,9 @@ class Base_View(discord.ui.View, ABC):
         
         :return discord.ui.Button | discord.ui.Select | discord.ui.UserSelect | discord.ui.RoleSelect | discord.ui.TextInput:
             Restored discord.ui element
+
+        :raises TypeError:
+            Tried to restore a unsupported component
         """
         element: Union[discord.ui.Button, discord.ui.Select, discord.ui.UserSelect, discord.ui.RoleSelect, discord.ui.MentionableSelect, discord.ui.TextInput]
         match component.type:
@@ -154,9 +278,9 @@ class Base_View(discord.ui.View, ABC):
         :raises StoppedBefore:
             View was stopped before but is now requested to start"""
         if self._active:
-            raise AlreadyActive(self._view_name, self._database_id, self.get_attributes())
+            raise AlreadyActive(self._view_name, self._database_id, self.get_attributes(unmangle_attribute_name = True))
         if self._stopped:
-            raise StoppedBefore(self._view_name, self._database_id, self.get_attributes())
+            raise StoppedBefore(self._view_name, self._database_id, self.get_attributes(unmangle_attribute_name = True))
         self._bot.active_views[self._message_id] = self
         self._active = True
 
@@ -170,9 +294,9 @@ class Base_View(discord.ui.View, ABC):
             View was never started
         """
         if self._stopped:
-            raise AlreadyStopped(self._view_name, self._database_id, self.get_attributes())
+            raise AlreadyStopped(self._view_name, self._database_id, self.get_attributes(unmangle_attribute_name = True))
         if not self._active:
-            raise NotActive(self._view_name, self._database_id, self.get_attributes())
+            raise NotActive(self._view_name, self._database_id, self.get_attributes(unmangle_attribute_name = True))
         del self._bot.active_views[self._message_id]
         self._active = False
         self._stopped = True
@@ -188,36 +312,3 @@ class Base_View(discord.ui.View, ABC):
         self.deactivate()
         await super().on_timeout()
         await self.save_state()
-
-    def get_attributes(self) -> dict[str, Any]:
-        """Returns all attributes set by this class and its parents
-        
-        :returns dict:
-            Dictionary containing all attributes with its key"""
-        result:dict[str, Any] = {}
-
-        # Traverse the class hierarchy (Method Resolution Order)
-        for cls in self.__class__.mro():
-            if cls is object:
-                continue  # Skip the base object class
-
-            class_name = cls.__name__
-
-            # Loop through all attributes stored directly on the instance
-            for attr in self.__dict__:
-                value = getattr(self, attr, None)
-
-                # Handle private attributes (name-mangled): __attr -> _Class__attr
-                if attr.startswith(f"_{class_name}__"):
-                    unmangled = attr.removeprefix(f"_{class_name}__")
-                    result[f"{class_name}.__{unmangled}"] = value
-
-                # Handle protected attributes: _attr
-                elif attr.startswith('_') and not attr.startswith('__'):
-                    result[f"{class_name}._{attr.lstrip('_')}"] = value
-
-                # Handle public attributes
-                else:
-                    result[f"{class_name}.{attr}"] = value
-
-        return result
--- src/views/exceptions.py
+++ src/views/exceptions.py
@@ -2,20 +2,12 @@ from abc import abstractmethod
 from typing import Any
 
 class GeneralViewException(Exception):
-    """Base class all exceptions during saving or restoring might throw"""
-    def __init__(self, view_name:str, database_id:int, attributes:dict[str, Any]):
+    """Base class for all exceptions, encounterd when using the restorable view"""
+    def __init__(self, view_name:str):
         """
         :param str view_name:
-            Name of the view class
-        
-        :param int | None database_id:
-            ID of the row in the database table, can be None if no state is saved
-
-        :param dict attributes:
-            All attributes the view has"""
+            Name of the view class"""
         self.name = view_name
-        self.database_id = database_id
-        self.attributes = attributes
 
     @abstractmethod
     def __str__(self):
@@ -41,25 +33,66 @@ class GeneralViewException(Exception):
         return ", ".join(f"{key}='{value}'" for key, value in keyword_arguments.items())
 
 
-class AlreadyStopped(GeneralViewException):
+class GeneralAttributeException(GeneralViewException):
+    """Base class for all exceptions, beling related with attributes"""
+    def __init__(self, view_name:str, attribute_name:str):
+        """
+        :param str view_name:
+            Name of the view class
+
+        :param str attribute_name:
+            Name of the attribute involved"""
+        super().__init__(view_name)
+        self.attribute_name = attribute_name
+
+
+class GeneralExtendedViewException(GeneralViewException):
+    """Extended base class all exceptions inherit, used when more data"""
+    def __init__(self, view_name:str, attributes:dict[str, Any]):
+        """
+        :param str view_name:
+            Name of the view class
+
+        :param dict attributes:
+            All attributes the view has"""
+        super().__init__(view_name)
+        self.attributes = attributes
+
+
+class GeneralStoredViewException(GeneralExtendedViewException):
+    """Base class specific to all exceptions, being thrown in combination with stored data by the database"""
+    def __init__(self, view_name:str, attributes:dict[str, Any], database_id:int):
+        """
+        :param str view_name:
+            Name of the view class
+
+        :param int | None database_id:
+            ID of the row in the database table, can be None if no state is saved
+
+        :param dict attributes:
+            All attributes the view has"""
+        super().__init__(view_name, database_id, attributes)
+
+
+class AlreadyStopped(GeneralStoredViewException):
     """Stopped view was requested to stop"""
     def __str__(self):
         return f"Attempt was made to stop view '{self.name}' that was stopped, database_id={self.database_id} and attributes {self.keyword_to_string(self.attributes)}"
 
 
-class StoppedBefore(GeneralViewException):
+class StoppedBefore(GeneralStoredViewException):
     """View has been stopped but is now requested to start"""
     def __str__(self):
         return f"Tried to start view '{self.name}' that was stopped before, database_id={self.database_id} and attributes {self.keyword_to_string(self.attributes)}"
 
 
-class AlreadyActive(GeneralViewException):
+class AlreadyActive(GeneralStoredViewException):
     """Activated view was requested to activate"""
     def __str__(self):
         return f"Attempt was made to start view '{self.name}' that was started, database_id={self.database_id} and attributes {self.keyword_to_string(self.attributes)}"
 
 
-class NotActive(GeneralViewException):
+class NotActive(GeneralStoredViewException):
     """Inactive view was requested to stop"""
     def __str__(self):
         return f"Attempt was made to stop view '{self.name}' that was never started, database_id={self.database_id} and attributes {self.keyword_to_string(self.attributes)}"
@@ -67,17 +100,27 @@ class NotActive(GeneralViewException):
 
 class CallbackNotFound(GeneralViewException):
     """No callback for the id was found"""
-    def __init__(self, view_name, database_id, attributes, custom_id:str):
+    def __init__(self, view_name:str, custom_id:str):
         """
         :param str view_name:
             Name of the view class
-        
-        :param int | None database_id:
-            ID of the row in the database table, can be None if no state is saved
 
-        :param dict attributes:
-            All attributes the view has
-            
         :param str custom_id:
             Custom id requested that could not be found"""
-        super().__init__(view_name, database_id, attributes)
+        super().__init__(view_name)
+        self.custom_id = custom_id
+
+    def __str__(self):
+        return f"The callback for the view '{self.name}' with the custom id '{self.custom_id}' was not found"
+
+
+class AttributeNameConflict(GeneralAttributeException):
+    """Attribute with same name was saved already"""
+    def __str__(self):
+        return f"While getting the attributes for the view '{self.name}', the value for the already saved attribute '{self.attribute_name}' was about to be overridden"
+
+
+class AttributeNotFound(GeneralAttributeException):
+    """Non-existent attribute was tried to set"""
+    def __str__(self):
+        return f"While restoring the attributes for the view '{self.name}', a non-existing attribute '{self.attribute_name}' was tried to be set"
